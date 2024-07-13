d13da2a547055cdcd98a2adeccb7504fc797731c
Ramanjot Singh <125717171+iamrommy@users.noreply.github.com>
2024-07-13 05:52:31 +0530

    Update apis.js

--- src/services/apis.js
+++ src/services/apis.js
@@ -2,9 +2,9 @@ const BASE_URL = process.env.REACT_APP_BASE_URL
 
 // AUTH ENDPOINTS
 export const endpoints = {
-  SENDOTP_API: BASE_URL + "/auth/sendotp",
-  SIGNUP_API: BASE_URL + "/auth/signup",
-  LOGIN_API: BASE_URL + "/auth/login",
-  GET_PROFILE: BASE_URL + "/auth/me",
-  LOGOUT_API: BASE_URL + "/auth/logout",
+  SENDOTP_API: BASE_URL + "/api/v1/auth/sendotp",
+  SIGNUP_API: BASE_URL + "/api/v1/auth/signup",
+  LOGIN_API: BASE_URL + "/api/v1/auth/login",
+  GET_PROFILE: BASE_URL + "/api/v1/auth/me",
+  LOGOUT_API: BASE_URL + "/api/v1/auth/logout",
 }
